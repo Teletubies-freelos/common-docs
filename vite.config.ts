3399c5b8dda988538ef44dc2d512798c4dca73f5
shadow <pieroramirez810@gmail.com>
2023-09-15 03:07:24 -0400

    fix: chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build:{
-    assetsDir: './'
-  }
+    assetsDir: './',
+    rollupOptions:{
+      output:{
+        manualChunks:{
+          react: ['react', 'react-dom'],
+          mermaid: ['mermaid'],
+          reactflow: ['reactflow'],
+          spectacle: ['spectacle']
+        }
+      }
+    }
+  },
 })
